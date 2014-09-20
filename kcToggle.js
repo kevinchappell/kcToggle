2c13bc5fb414ca4851364777a7cb91124f1bab9d
Kevin Chappell <kevin.b.chappell@gmail.com>
2014-09-19 20:55:05 -0700

    Initial commit

---
+++ kcToggle.js
@@ -0,0 +1,62 @@
+/**
+ * kcToggle
+ * Description: Replace the default browser checkbox with a styled toggle
+ * Version: .1
+ * Author: Kevin Chappell
+ * Dependencies: jQuery Core v1.6+
+ */
+
+(function ($) {
+  var Toggle = function (element, options) {
+
+    var defaults = {
+      theme: 'fresh',
+      text: {
+        off: uiLabel.off,
+        on: uiLabel.on
+      }
+    };
+
+    var utils = {
+      toggleChecked: function (elem) {
+        elem.attr('checked', !elem.attr('checked'));
+      }
+    };
+
+    var opts = $.extend(defaults, options),
+      elem = $(element);
+
+    if (elem.attr('checked') == 'checked') {
+      elem.wrap('<div class="kcToggle on" />');
+    } else {
+      elem.wrap('<div class="kcToggle off" />');
+    }
+
+    elem.parent('div.kcToggle').append('<div class="kcToggleInner"><div class="kcToggleOn">' + opts.text.on + '</div><div class="kcToggleHandle"></div><div class="kcToggleOff">' + opts.text.off + '</div></div>');
+
+    elem.parent('div.kcToggle').click(function () {
+      utils.toggleChecked(elem);
+      $(this).toggleClass('on');
+      $(this).toggleClass('off');
+    });
+
+  }
+
+
+  $.fn.kcToggle = function (options) {
+    var toggle = this;
+    return toggle.each(function (i) {
+      var element = $(this);
+      if (element.data('kcToggle')) return;
+      var kcToggle = new Toggle(this, options);
+      element.data('kcToggle', kcToggle);
+
+      if ($.browser.msie && parseInt($.browser.version, 10) < 9) {
+        // custom styling for old IE
+        var ie_style = '<style type="text/css"></style>';
+        element.after(ie_style);
+      }
+    });
+  };
+
+})(jQuery);

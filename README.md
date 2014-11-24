210cc55014f136c4ee7212d626626a61adcc7b03
Kevin Chappell <kevin.b.chappell@gmail.com>
2014-11-23 17:11:11 -0800

    added example, minor style update

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-kcToggle
+# kcToggle
 ========
 
-Replace default browser checkboxes with this customizable jQuery plugin
+Replace default browser checkboxes with this themable jQuery plugin.
+
+## Install
+In your html document simply add the following to your `<head>`. *kcToggle* is jQuery dependant so be sure to have a copy of jQuery 1.6+ loaded before including the plugin files in your document's `<head>`.
+```
+  <link   href="../kcToggle.css"  media="all" rel="stylesheet" type="text/css" />
+  <script src="../kcToggle.js"    type="text/javascript" ></script>
+```
+
+## Usage
+*kcToggle* can be used on any checkbox. To initialize the plugin and get your checkboxes looking like the one below call the plugin like so: `$('input[kcToggle]').kcToggle();`. kcToggle will also take options. You can set the label it displays by passing the option in an Object like below.
+```
+var labels = {
+      text: {
+        off: 'No',
+        on: 'Yes'
+      }
+    };
+$('input[kcToggle]').kcToggle(labels);
+```
--- kcToggle.js
+++ kcToggle.js
@@ -6,19 +6,19 @@
  * Dependencies: jQuery Core v1.6+
  */
 
-(function ($) {
-  var Toggle = function (element, options) {
+(function($) {
+  var Toggle = function(element, options) {
 
     var defaults = {
       theme: 'fresh',
       text: {
-        off: uiLabel.off,
-        on: uiLabel.on
+        off: 'Off',
+        on: 'On'
       }
     };
 
     var utils = {
-      toggleChecked: function (elem) {
+      toggleChecked: function(elem) {
         elem.attr('checked', !elem.attr('checked'));
       }
     };
@@ -26,7 +26,7 @@
     var opts = $.extend(defaults, options),
       elem = $(element);
 
-    if (elem.attr('checked') == 'checked') {
+    if (elem.attr('checked') === 'checked') {
       elem.wrap('<div class="kcToggle on" />');
     } else {
       elem.wrap('<div class="kcToggle off" />');
@@ -34,20 +34,22 @@
 
     elem.parent('div.kcToggle').append('<div class="kcToggleInner"><div class="kcToggleOn">' + opts.text.on + '</div><div class="kcToggleHandle"></div><div class="kcToggleOff">' + opts.text.off + '</div></div>');
 
-    elem.parent('div.kcToggle').click(function () {
+    elem.parent('div.kcToggle').click(function() {
       utils.toggleChecked(elem);
       $(this).toggleClass('on');
       $(this).toggleClass('off');
     });
 
-  }
+  };
 
 
-  $.fn.kcToggle = function (options) {
+  $.fn.kcToggle = function(options) {
     var toggle = this;
-    return toggle.each(function (i) {
+    return toggle.each(function() {
       var element = $(this);
-      if (element.data('kcToggle')) return;
+      if (element.data('kcToggle')) {
+        return;
+      }
       var kcToggle = new Toggle(this, options);
       element.data('kcToggle', kcToggle);
 
